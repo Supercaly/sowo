6f3f8196384af8dfc1ba4b7eb912e22a9a07aad0
Lorenzo Calisti <djonline96@gmail.com>
2021-04-27 12:13:37 +0200

    Add CompilerOptions and command line arguments parsing

--- main.go
+++ main.go
@@ -18,20 +18,47 @@ func readFileAsString(filePath string) string {
 }
 
 func main() {
-	filePath := os.Args[1]
-	inputFile := readFileAsString(filePath)
+	options := sowo.OptionsFromCommandLine()
+
+	// Read the input file
+	inputFile := readFileAsString(options.InputFile)
 
 	reporter := sowo.Reporter{
 		Input:    inputFile,
-		FileName: filePath}
+		FileName: options.InputFile}
+
+	// Start the compilation process:
+	// Tokenize the file
 	lexer := sowo.Lexer{
 		Input:    sowo.Input(inputFile),
 		Reporter: reporter}
 	lexer.Tokenize()
-	lexer.DumpTokens()
-	fmt.Println()
+	if options.PrintTokens {
+		lexer.DumpTokens()
+		fmt.Println()
+	}
 
+	// Parse the tokens
 	parser := sowo.Parser{Tokens: lexer.Tokens, Reporter: reporter}
-	module := parser.ParseModule()
-	fmt.Println(module)
+	ast := parser.ParseModule()
+	if options.PrintAst {
+		sowo.DumpAst(ast, 0)
+	}
+
+	if !options.SkipCompile {
+		// Compile the Ast to assembly code
+		asFrontend := sowo.AsFrontend{}
+		assembly := asFrontend.AsF(ast)
+
+		fmt.Println()
+		fmt.Println("Assembly:")
+		fmt.Println(assembly)
+
+		outPath := os.Args[2]
+		f, err := os.Create(outPath)
+		if err != nil {
+			log.Fatalf("Error opening file %s", outPath)
+		}
+		f.WriteString(assembly)
+	}
 }
---
+++ src/options.go
@@ -0,0 +1,72 @@
+package src
+
+import (
+	"fmt"
+	"os"
+)
+
+// Represents a set of options used by the compiler.
+type CompilerOptions struct {
+	PrintTokens bool
+	PrintAst    bool
+	SkipCompile bool
+	InputFile   string
+	OutputFile  string
+}
+
+// Creates new CompilerOptions from command line arguments.
+func OptionsFromCommandLine() CompilerOptions {
+	args := os.Args
+	options := CompilerOptions{SkipCompile: true}
+
+	for i := 1; i < len(args); i++ {
+		if args[i] == "-o" || args[i] == "--output" {
+			if i == len(args)-1 {
+				fmt.Println("--output flag must be followed by a file name")
+				usage()
+				os.Exit(1)
+			}
+			options.OutputFile = args[i+1]
+			i++
+			continue
+		}
+		if args[i] == "-t" || args[i] == "--print-tokens" {
+			options.PrintTokens = true
+			continue
+		}
+		if args[i] == "-p" || args[i] == "--print-ast" {
+			options.PrintAst = true
+			continue
+		}
+		if args[i] == "-n" || args[i] == "--no-compile" {
+			options.SkipCompile = true
+			continue
+		}
+		if args[i] == "-h" || args[i] == "--help" {
+			usage()
+			os.Exit(0)
+		}
+		options.InputFile = args[i]
+	}
+
+	if len(options.InputFile) == 0 {
+		fmt.Println("An input file must be specified!")
+		usage()
+		os.Exit(1)
+	}
+
+	return options
+}
+
+// Prints the program usage to stdout
+func usage() {
+	fmt.Println("Usage: main.go [options...] [input.sowo]")
+	fmt.Println()
+	fmt.Println("Options:")
+	fmt.Println(" -o, --output out.asm : Specify the output filename.")
+	fmt.Println(" -t, --print-tokens   : Print the tokens.")
+	fmt.Println(" -p, --print-ast      : Print the AST.")
+	fmt.Println(" -n, --no-compile     : Stop the process before the compilation step.")
+	fmt.Println(" -h, --help           : Prints this help message.")
+	fmt.Println()
+}
