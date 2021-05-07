5330b0084185a09e97c11457c656e832b8de676a
Lorenzo <djonline96@gmail.com>
2021-05-07 18:24:37 +0200

    Fix type checker

--- src/type_checker.go
+++ src/type_checker.go
@@ -6,30 +6,19 @@ import (
 )
 
 type Scope struct {
-	vars []DefInterface
+	vars []VarDef
 }
 
 var scopes []Scope
 
-var currentFuncDef *Ast = nil
+var currentModule *Ast = nil
 
-type DefInterface interface {
-	interfaceMethod()
-}
+var currentFuncDef *Ast = nil
 
 type VarDef struct {
 	Name string
 	Type TypeAnnotation
 }
-
-type FuncDef struct {
-	Name       string
-	ReturnType TypeAnnotation
-	Params     []VarDef
-}
-
-func (vd *VarDef) interfaceMethod()  {}
-func (fd *FuncDef) interfaceMethod() {}
 
 func (s Scope) String() string {
 	return fmt.Sprintf("Scope{vars: %s}", s.vars)
@@ -39,25 +28,18 @@ func (vd VarDef) String() string {
 	return fmt.Sprintf("VarDef{Name: %s, Type: %s}", vd.Name, vd.Type.String())
 }
 
-func (fd FuncDef) String() string {
-	return fmt.Sprintf("FuncDef{Name: %s, ReturnType: %s, Params: %s}", fd.Name, fd.ReturnType.String(), fd.Params)
-}
-
 func pushScope(ast *Ast) {
+	var newScope Scope
 	if ast != nil {
-		var params []VarDef
+		var scopeVars []VarDef
 		for _, p := range ast.Children[0].Children {
-			params = append(params, VarDef{Name: p.Name, Type: p.Children[0].DataType})
+			scopeVars = append(scopeVars, VarDef{Name: p.Name, Type: p.Children[0].DataType})
 		}
-		newScope := Scope{vars: make([]DefInterface, 1)}
-		newScope.vars[0] = &FuncDef{
-			Name:       ast.Name,
-			ReturnType: ast.Children[1].Children[0].DataType,
-			Params:     params}
-		scopes = append(scopes, newScope)
+		newScope = Scope{vars: scopeVars}
 	} else {
-		scopes = append(scopes, Scope{})
+		newScope = Scope{}
 	}
+	scopes = append(scopes, newScope)
 	fmt.Println("push", scopes)
 }
 
@@ -67,25 +49,16 @@ func popScope() {
 }
 
 func pushVarDef(ast Ast) {
-	varDef := &VarDef{Name: ast.Name, Type: ast.Children[0].DataType}
+	varDef := VarDef{Name: ast.Name, Type: ast.Children[0].DataType}
 	scopes[len(scopes)-1].vars = append(scopes[len(scopes)-1].vars, varDef)
-	fmt.Println("pushv", scopes)
+	fmt.Println("push_var", scopes)
 }
 
 func typeOfVarWithName(name string) (TypeAnnotation, error) {
 	for _, scope := range scopes {
 		for _, vars := range scope.vars {
-			switch v := vars.(type) {
-			case *FuncDef:
-				for _, p := range v.Params {
-					if p.Name == name {
-						return p.Type, nil
-					}
-				}
-			case *VarDef:
-				if v.Name == name {
-					return v.Type, nil
-				}
+			if vars.Name == name {
+				return vars.Type, nil
 			}
 		}
 	}
@@ -93,19 +66,47 @@ func typeOfVarWithName(name string) (TypeAnnotation, error) {
 }
 
 func typeOfFuncWithName(name string) (TypeAnnotation, error) {
-	for _, scope := range scopes {
-		for _, vars := range scope.vars {
-			switch v := vars.(type) {
-			case *FuncDef:
-				if v.Name == name {
-					return v.ReturnType, nil
-				}
-			case *VarDef:
-			}
-
+	if currentModule == nil {
+		log.Fatal("[Type Check]: No module found")
+	}
+	for _, funcDef := range currentModule.Children {
+		if funcDef.Name == name {
+			return funcDef.Children[1].Children[0].DataType, nil
 		}
 	}
 	return TypeVoid, fmt.Errorf("no function with name %s", name)
+}
+
+func typeOfExpression(ast Ast) (ret TypeAnnotation, err error) {
+	switch ast.Type {
+	case AstNumberLiteral:
+		ret = TypeInteger
+	case AstBooleanLiteral:
+		ret = TypeBoolean
+	case AstStringLiteral:
+		ret = TypeString
+	case AstFuncCall:
+		ret, err = typeOfFuncWithName(ast.Name)
+	case AstVariableRef:
+		ret, err = typeOfVarWithName(ast.Name)
+	case AstBinaryOp:
+		lhsType, lErr := typeOfExpression(ast.Children[0])
+		if lErr != nil {
+			return TypeVoid, lErr
+		}
+		rhsType, rErr := typeOfExpression(ast.Children[1])
+		if rErr != nil {
+			return TypeVoid, rErr
+		}
+		if lhsType != rhsType {
+			err = fmt.Errorf("left operand has type '%s' right operand has type '%s'",
+				lhsType, rhsType)
+		}
+		ret = lhsType
+	default:
+		log.Fatalf("[Type Check]: Unsupported expression '%s'", ast.Type)
+	}
+	return ret, err
 }
 
 func checkTypeOfFuncCall(ast Ast, expectedType TypeAnnotation) {
@@ -118,15 +119,55 @@ func checkTypeOfFuncCall(ast Ast, expectedType TypeAnnotation) {
 	}
 }
 
+func checkTypeOfBinaryOp(ast Ast, expectedType TypeAnnotation) {
+	lhsType, lErr := typeOfExpression(ast.Children[0])
+	rhsType, rErr := typeOfExpression(ast.Children[1])
+
+	if lErr != nil {
+		log.Fatalf("[Type Check]: Binary operation: %s", lErr)
+	}
+	if rErr != nil {
+		log.Fatalf("[Type Check]: Binary operation: %s", rErr)
+	}
+	fmt.Println(lhsType, rhsType)
+	switch ast.Operator {
+	case OpPlus, OpMinus, OpTimes, OpDivide,
+		OpEquals, OpLessThen, OpGreaterThen, OpLessThenEqual, OpGreaterThenEqual:
+		if lhsType != rhsType {
+			log.Fatalf("[Type Check]: Binary operation mismatch: left operand has type '%s' right operand has type '%s'",
+				lhsType, rhsType)
+		}
+	default:
+		log.Fatalf("[Type Check]: Unsupported binary operator '%s'", ast.Operator)
+	}
+}
+
 func checkTypeOfExpression(ast Ast, expectedType TypeAnnotation) {
 	switch ast.Type {
 	case AstNumberLiteral:
 		if expectedType != TypeInteger {
 			log.Fatalf("[Type Check]: Expected type '%s' but expression has type '%s'", expectedType, TypeInteger)
 		}
+	case AstBooleanLiteral:
+		if expectedType != TypeBoolean {
+			log.Fatalf("[Type Check]: Expected type '%s' but expression has type '%s'", expectedType, TypeBoolean)
+		}
+	case AstStringLiteral:
+		if expectedType != TypeString {
+			log.Fatalf("[Type Check]: Expected type '%s' but expression has type '%s'", expectedType, TypeString)
+		}
 	case AstFuncCall:
 		checkTypeOfFuncCall(ast, expectedType)
+	case AstVariableRef:
+		varType, err := typeOfVarWithName(ast.Name)
+		if err != nil {
+			log.Fatal(err)
+		}
+		if varType != expectedType {
+			log.Fatalf("[Type Check]: Expected variable reference with type '%s' but got '%s'", expectedType, varType)
+		}
 	case AstBinaryOp:
+		checkTypeOfBinaryOp(ast, expectedType)
 	default:
 		log.Fatalf("[Type Check]: Unsupported expression '%s'", ast.Type)
 	}
@@ -146,8 +187,30 @@ func checkTypeOfLocalVar(ast Ast) {
 	pushVarDef(ast.Children[0])
 }
 
+func checkTypeOfIf(ast Ast, expectedType TypeAnnotation) {
+	checkTypeOfExpression(ast.Children[0], TypeBoolean)
+	checkTypeOfBlock(ast.Children[1], expectedType)
+	if len(ast.Children) == 3 {
+		checkTypeOfBlock(ast.Children[2], expectedType)
+	}
+}
+
+func checkTypeOfWhile(ast Ast, expectedType TypeAnnotation) {
+	checkTypeOfExpression(ast.Children[0], TypeBoolean)
+	checkTypeOfBlock(ast.Children[1], expectedType)
+}
+
 func checkTypeOfReturn(ast Ast, expectedType TypeAnnotation) {
 	checkTypeOfExpression(ast.Children[0], expectedType)
+}
+
+func checkTypeOfPrint(ast Ast) {
+	for _, expr := range ast.Children {
+		_, err := typeOfExpression(expr)
+		if err != nil {
+			log.Fatalf("[Type Check]: %s", err)
+		}
+	}
 }
 
 func checkTypeOfStatement(ast Ast, expectedType TypeAnnotation) {
@@ -158,6 +221,12 @@ func checkTypeOfStatement(ast Ast, expectedType TypeAnnotation) {
 		checkTypeOfAssignment(ast)
 	case AstReturn:
 		checkTypeOfReturn(ast, expectedType)
+	case AstIf:
+		checkTypeOfIf(ast, expectedType)
+	case AstWhile:
+		checkTypeOfWhile(ast, expectedType)
+	case AstPrint:
+		checkTypeOfPrint(ast)
 	default:
 		log.Fatalf("[Type Check]: Unsupported statements '%s'", ast.Type)
 	}
@@ -191,6 +260,7 @@ func checkTypeOfFunction(ast Ast) {
 }
 
 func checkTypeOfModule(ast Ast) {
+	currentModule = &ast
 	for _, funcDef := range ast.Children {
 		switch funcDef.Type {
 		case AstFunction:
